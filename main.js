d28947d47c35edd0c65b38b66acf62cc9d980a5a
Serg Skol <sps676@gmail.com>
2022-06-02 19:58:24 +0200

    input txt

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function main() {
+function main(input_text) {
     const result = "Hello";
+
     console.log(result);
-    alert(result);
+    //alert(result);
+    //document.write(result);
+    document.getElementById('result').innerHTML = input_text+" : "+result;
 }
