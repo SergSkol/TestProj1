130387fce0f92a7df9e91305e4bd50397c103399
Serg Skol <sps676@gmail.com>
2023-10-20 09:11:43 +0200

    Add ts case

--- javaScript/test.js
+++ javaScript/test.js
@@ -1,19 +1,12 @@
-const increaseValueByKey = (keyValues, key) => {
-  if (keyValues[key]) {
-    keyValues[key] = keyValues[key] + 1;
-  } else {
-    keyValues[key] = 1;
+let customersArray = ['Custy Stomer', 'C. Oostomar', 'C.U.S. Tomer', 3432434, 'Custo Mer', 'Custopher Ustomer', 3432435, 'Kasti Yastimeur'];
+
+//Write Your Code here:
+function checkCustomersArray(arr) {
+  for (let el of arr) {
+    if (typeof el == "number") {
+      console.log(`Type error: ${el} should be a string!`)
+    }
   }
-
 }
 
-const keyValues = {};
-increaseValueByKey(keyValues, 1);
-increaseValueByKey(keyValues, 1);
-increaseValueByKey(keyValues, 2);
-
-console.log(keyValues);
-
-for (let key in keyValues) {
-  console.log("key " + key + " = " + keyValues[key]);
-}
+checkCustomersArray(customersArray);
